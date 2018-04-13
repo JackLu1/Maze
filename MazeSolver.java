7a49198575ffb5322921bea2ea5f8c8207224b0c
Jack Lu <jlu2025@gmail.com>
2018-04-12 20:40:34 -0400

    Work in progress

--- MazeSolver.java
+++ MazeSolver.java
@@ -6,28 +6,48 @@ Determine whether there is a path to the treasure in the Maze from the starting 
 public class MazeSolver {
 
      public Maze toSolve;
-    
+      
      public MazeSolver(Maze maze){
 	 toSolve = maze;
      }
     
      public boolean solve(){
-	 if (toSolve.explorerIsOnA() == 0){
-	     
-	 System.out.println("true              ");
-	     return true;
-	 }
-	 else if (toSolve.explorerIsOnA() == 1){
-	     
-	 System.out.println("false         ");
-	     return false;
-	 }
-	 else {
-	     System.out.println("recursion begins");
-	     
-	 
-	 return true;
-	 }
+
+         Maze snapshot = new Maze( toSolve);
+
+		 if (toSolve.explorerIsOnA() == 0){
+		     
+		 System.out.println("true              ");
+            System.out.println( toSolve);            
+		    return true;
+		 }
+		 else if (toSolve.explorerIsOnA() == 1){
+		     
+		    System.out.println("backtrack");
+            System.out.println( toSolve); 
+
+            toSolve = snapshot;
+		 }
+		 else {
+		    System.out.println("recursion");
+
+            toSolve.dropA( Maze.WALL);
+            toSolve.go( toSolve.EAST);       
+            revertTest( snapshot); 
+
+            System.out.println( toSolve.toString());
+
+            //recursion
+            solve(); 
+    	 }
+         return false;
+     }
+
+     private void revertTest( Maze snapshot){
+        int element = toSolve.explorerIsOnA();
+        if (element == 1){
+           toSolve = snapshot; 
+        }
      }
 
 }
