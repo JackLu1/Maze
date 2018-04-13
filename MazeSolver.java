16cbd8213b7818a798805b7e23e25b4a03a25e5b
Jack Lu <jlu2025@gmail.com>
2018-04-12 22:30:26 -0400

    finished v0

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,53 +1,42 @@
 /**
-Determine whether there is a path to the treasure in the Maze from the starting position of the explorer or there is no solution 
-*/
+ * Determine whether there is a path to the treasure in the Maze from the starting position of the explorer or there is no solution
+ */
 
 
 public class MazeSolver {
 
-     public Maze toSolve;
-      
-     public MazeSolver(Maze maze){
-	 toSolve = maze;
-     }
-    
-     public boolean solve(){
+	public Maze toSolve;
+	private int[] directions = new int[]{ Maze.EAST, Maze.SOUTH, Maze.WEST, Maze.NORTH, };
 
-         Maze snapshot = new Maze( toSolve);
+	public MazeSolver(Maze maze) {
+		toSolve = maze;
+	}
 
-		 if (toSolve.explorerIsOnA() == 0){
-		     
-		 System.out.println("true              ");
-            System.out.println( toSolve);            
-		    return true;
-		 }
-		 else if (toSolve.explorerIsOnA() == 1){
-		     
-		    System.out.println("backtrack");
-            System.out.println( toSolve); 
+	public boolean solve() {
 
-            toSolve = snapshot;
-		 }
-		 else {
-		    System.out.println("recursion");
 
-            toSolve.dropA( Maze.WALL);
-            toSolve.go( toSolve.EAST);       
-            revertTest( snapshot); 
-
-            System.out.println( toSolve.toString());
-
-            //recursion
-            solve(); 
-    	 }
-         return false;
-     }
-
-     private void revertTest( Maze snapshot){
-        int element = toSolve.explorerIsOnA();
-        if (element == 1){
-           toSolve = snapshot; 
-        }
-     }
-
+		if (toSolve.explorerIsOnA() == 0) {
+//			System.out.println("true");
+//			System.out.println(toSolve);
+			return true;
+		}
+		else if (toSolve.explorerIsOnA() == 1) {
+//			System.out.println(" fail. on wall");
+//			System.out.println(toSolve);
+		}
+		else {
+//			System.out.println("recursion");
+			for (int i = 0; i < 4; i++){
+				Maze snapshot = new Maze(toSolve);
+				toSolve.dropA( Maze.WALL);
+//				System.out.println( toSolve);
+				toSolve.go( directions[i]);
+				if ( solve() )
+					return true;
+				toSolve = snapshot;
+			}
+		}
+//		System.out.println(" fail no solution");
+		return false;
+	}
 }
--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -3,7 +3,7 @@
  * Requires command line arguments:
  * o  the name of a file containing a maze.
  * o  the rank and file where an explorer is starting
- * 
+ *
  * For example,
  * java UserOfMazeSolver mazes/4cell_treasureWest.txt -1 -1  # no explorer
  * credit to Mr. Holmes!
@@ -21,8 +21,7 @@ public class UserOfMazeSolver {
 				, Integer.parseInt(commandLine[2])
 		);
 		MazeSolver solver = new MazeSolver(maze);
-		solver.solve();
-	        
+		System.out.println( solver.solve());
 
 	}
 }
